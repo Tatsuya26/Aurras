67c94958e639d8e4bb8ea5136f350d7c452ef019
DavidPereiraAlves <a93274@alunos.uminho.pt>
2021-06-13 17:19:26 +0100

    criacao do servidor

--- src/aurras.c
+++ src/aurras.c
@@ -1,3 +1,22 @@
-int main() {
-    return 0;
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(int argc,char *argv[]) {
+    if (argc == 1) {
+        write (1,"./aurras status\n./aurras transform input-filename output-filename filter-id1 filter-id2 ...\n",92);
+    }
+    else if (argc == 2 && strcmp(argv[1],"status") == 0) {
+        write (1,"estado do servidor\n",19);
+    }
+    else if (argc >= 4 && strcmp(argv[1],"transform") == 0) {
+        write (1,"processar ficheiro\n",19);
+    }
+    else write (1,"Comando nao reconhecido!\n",25);
 }
--- src/aurrasd.c
+++ src/aurrasd.c
@@ -1,3 +1,59 @@
-int main() {
-    return 0;
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+
+ssize_t readln (int fd, char *buffer,size_t size) {
+	ssize_t res = 0;
+	ssize_t j = 0;
+	char local[1];
+
+	while ((res = read (fd,local,1)) > 0) {
+		if (local[0] != '\n') {
+				buffer[j] = local[0];
+				j++;
+			}
+			else {
+				buffer[j] = local[0];
+				j++;
+				return j;
+			}
+		}
+	return j;
+}
+
+int getConf (int fdConf,char *filterID[],char *filterFile[],int filterLimit[]) {
+    char* buffer = malloc (100);
+    int numFiltros;
+    for (numFiltros = 0; readln(fdConf,buffer,100) > 0 && numFiltros < 10; numFiltros++) {
+        filterID[numFiltros] = strsep(&buffer," ");
+        filterFile[numFiltros] = strsep(&buffer," ");
+        filterLimit[numFiltros] = atoi(strsep(&buffer," "));
+        buffer = malloc(100);
+    }
+    return numFiltros;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 3) {
+        int fdConf,numTarefas;
+        if ((fdConf = open(argv[1],O_RDONLY)) == -1) perror("");
+        else {
+        int numFiltros;
+        char* filterID[10];
+        char* filterFile[10];
+        int filterLimit[10];
+        numFiltros = getConf (fdConf,filterID,filterFile,filterLimit);
+        mkfifo("servidor",0666);
+        //while(1) {
+            //int pr = open("servidor",O_RDONLY);
+        //}
+        }
+    }
 }
